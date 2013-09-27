3b3c0cbbd047edca6b3cd48fd3d9750d32917cee
Julien Jorge <julien.jorge@stuff-o-matic.com>
2013-09-27 23:45:15 +0200

    The mouse detector toggles only on finger pressure.

--- bear-engine/lib/src/generic_items/code/mouse_detector.cpp
+++ bear-engine/lib/src/generic_items/code/mouse_detector.cpp
@@ -149,7 +149,9 @@ bool bear::mouse_detector::finger_action( const input::finger_event& event )
        ( get_level().screen_to_level( event.get_position() ) ) )
     {
       result = true;
-      trigger_toggle();
+
+      if ( event.get_type() == input::finger_event::finger_event_pressed )
+        trigger_toggle();
     }
 
   return result;
