9f414a2de1c26142218393b1cd8e2692f829d1e8
Julien Jorge <julien.jorge@stuff-o-matic.com>
2013-12-30 23:10:57 +0100

    Add a level to be launched by the sample program.

--- bear-engine/doc/get-started/project-sample/launcher/main.cpp
+++ bear-engine/doc/get-started/project-sample/launcher/main.cpp
@@ -8,7 +8,7 @@ int main( int argc, char* argv[] )
   game_description.set_game_name( "Sample project" );
   game_description.set_screen_width( 640 );
   game_description.set_screen_height( 480 );
-  game_description.add_resources_path( "./data" );
+  game_description.add_resources_path( "./resources" );
   game_description.add_item_library( "libbear_generic_items.so" );
   game_description.set_start_level( "level/start.cl" );
 
