7242a123ddedc14276fa96eb5d1575bdccfb8e7f
Julien Jorge <julien.jorge@stuff-o-matic.com>
2014-01-05 15:21:56 +0100

    Add an example program showing how to play sounds.

--- bear-engine/doc/examples/9-sound/main.cpp
+++ bear-engine/doc/examples/9-sound/main.cpp
@@ -1,7 +1,7 @@
 /**
  * \file
  *
- * This example program creates a window and plays a music.
+ * This example program creates a window and plays two sounds randomly.
  *
  * In order to make this example minimalistic, it will not use most of the
  * functionalities of the Bear Engine. Consequently, we will have manage the
@@ -13,6 +13,7 @@
 
 #include "visual/screen.hpp"
 #include "audio/sound_manager.hpp"
+#include "time/time.hpp"
 
 #include "SDL2/SDL.h"
 
@@ -72,13 +73,29 @@ void release()
 }
 
 /**
- * Creates a window and plays the music in a loop until the quit signal is
+ * Loads a sound from a given file name.
+ * \param sound_manager The sound manager in which the sound is loaded.
+ * \param file_name The path to the sound file to open.
+ * \param sound_name The name to give to the sound.
+ */
+void load_sound
+( bear::audio::sound_manager& sound_manager, const std::string& file_name,
+  const std::string& sound_name )
+{
+  std::ifstream f( file_name );
+  sound_manager.load_sound( sound_name, f );
+}
+
+/**
+ * Creates a window and plays the sounds randomly until the quit signal is
  * received.
  */
 void run_example()
 {
+  init();
+
   // A window must be created to initialize the SDL. We do not need it for the
-  // music but for the events that will allow to close the application.
+  // sound but for the events that will allow to close the application.
   //
   // Here we create it; it will be immediately visible.
   // The engine does not support multiples windows yet, so be careful and be
@@ -87,24 +104,35 @@ void run_example()
   // As soon as the screen is created, a render thread is created 
   bear::visual::screen s( claw::math::coordinate_2d<unsigned int>(640, 480) );
 
-  // Musics must be accessed through a sound manager. It will handle the loading
-  // of the files and the releasing of the resources when the music is over.
+  // Sounds must be accessed through a sound manager. It will handle the loading
+  // of the files and the releasing of the resources when the sound is over.
   bear::audio::sound_manager sound_manager;
 
-  // We need to load the music into the sound manager before being able to play
+  // We need to load the sound into the sound manager before being able to play
   // it.
-  std::ifstream f( "mus_1.ogg" );
-  sound_manager.load_sound("music", f);
-
-  std::cout << "Music by Alexandr Zhelanov. See "
-    "http://opengameart.org/content/mus.\n";
-
-  // Here we start the music. The second argument is the number of times it must
-  // be played. Zero means to play it forever.
-  sound_manager.play_music( "music", 0 );
+  load_sound( sound_manager, "lava.ogg", "lava" );
+  load_sound( sound_manager, "acid_burn.ogg", "acid" );
+    
+  std::cout << "Sound 'lava.ogg' by Michel Baradari. See "
+    "http://opengameart.org/content/lava-splash.\n"
+    "Sound 'acid_burn.ogg' by mikhog, based on work by Micher Bardari. See "
+    "http://opengameart.org/content/acid-burn-sounds.\n";
 
   while ( !g_quit )
-    SDL_PumpEvents();
+    {
+      SDL_PumpEvents();
+
+      const double lottery( (double)std::rand() / RAND_MAX );
+
+      // Here we play one of the sounds once.
+      if ( lottery < 0.3 )
+        sound_manager.play_sound( "lava" );
+      else if ( lottery < 0.6 )
+        sound_manager.play_sound( "acid" );
+        
+      // wait 1 second before the next sound.
+      bear::systime::sleep( 1000 );
+    }
 }
 
 /**
