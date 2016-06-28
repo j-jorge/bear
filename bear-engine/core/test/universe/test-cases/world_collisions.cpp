d1f61df3d6fed04d9ed31e995de449a5a0d5c7cc
Julien Jorge <julien.jorge@stuff-o-matic.com>
2016-06-28 22:21:55 +0200

    Add a test for successive collisions.

--- bear-engine/core/test/universe/test-cases/world_collisions.cpp
+++ bear-engine/core/test/universe/test-cases/world_collisions.cpp
@@ -185,3 +185,67 @@ BOOST_AUTO_TEST_CASE( chain_of_four )
   BOOST_CHECK( collisionCD );
   BOOST_CHECK( !collisionBC );
 }
+
+BOOST_AUTO_TEST_CASE( successive_collisions )
+{
+  bear::universe::world world( test::g_world_size );
+  world.set_gravity( bear::universe::force_type( 0, 0 ) );
+  
+  test::universe::item_mockup itemA;
+  itemA.set_size( 10, 10 );
+  itemA.set_center_of_mass( 100, 100 );
+  itemA.set_mass( 50 );
+  world.register_item( &itemA );
+  
+  test::universe::item_mockup itemB;
+  itemB.set_size( 10, 10 );
+  itemB.set_center_of_mass( 90, 90 );
+  itemB.set_mass( 50 );
+  world.register_item( &itemB );
+
+  test::universe::item_mockup itemC;
+  itemC.set_size( 10, 10 );
+  itemC.set_center_of_mass( 110, 110 );
+  itemC.set_mass( 50 );
+  world.register_item( &itemC );
+
+  itemB.time_step_impl =
+    [ &itemB ]( bear::universe::time_type ) -> void
+    {
+      itemB.set_center_of_mass( 100, 100 );
+    };
+
+  itemA.collision_impl =
+    []( bear::universe::collision_info& info ) -> void
+    {
+      info.other_item().set_center_of_mass( 110, 110 );
+    };
+
+  bool collisionBA( false );
+  bool collisionBC( false );
+  bool collisionCA( false );
+
+  itemB.collision_impl =
+    [ & ]( bear::universe::collision_info& info ) -> void
+    {
+      if ( &info.other_item() == &itemA )
+        collisionBA = true;
+      else if ( &info.other_item() == &itemC )
+        collisionBC = true;
+    };
+    
+  itemC.collision_impl =
+    [ & ]( bear::universe::collision_info& info ) -> void
+    {
+      if ( &info.other_item() == &itemA )
+        collisionCA = true;
+      else if ( &info.other_item() == &itemB )
+        collisionBC = true;
+    };
+    
+  world.progress_entities( test::g_update_region, 1 );
+
+  BOOST_CHECK( collisionBA );
+  BOOST_CHECK( !collisionCA );
+  BOOST_CHECK( collisionBC );
+}
